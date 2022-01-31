3d79b93301899b523776a105b1a9dd99fb461170
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-01-31 22:13:52 +0600

    bugs handled

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Triangle triangle = new Triangle();
+        triangle.a = 5;
+        triangle.b = 6;
+        triangle.c =7;
         triangle.area();
 
     }
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -3,19 +3,14 @@ package com.company;
 import java.util.Scanner;
 
 public class Triangle {
+
     int a,b,c;
 
-    static void area(){    //formula of area =  (a+b+c)/2 = s ==> sqrt s*(s-a)*(s-b)*(s-c)
-        Triangle triangle = new Triangle();
-        System.out.println("enter the first side of triangle");
-        Scanner scanner = new Scanner(System.in);
-        triangle.a = scanner.nextInt();
-        System.out.println("enter the second side of triangle");
-        triangle.b = scanner.nextInt();
-        System.out.println("enter the third side of triangle");
-        triangle.c = scanner.nextInt();
-        double s = (triangle.a+ triangle.b+ triangle.c)/(double)2;
-        double area = Math.sqrt(s*(s-triangle.a)*(s-triangle.b)*(s-triangle.c));
+    public void area(){   //formula of area =  (a+b+c)/2 = s ==> sqrt s*(s-a)*(s-b)*(s-c)
+
+        double s = (a+b+c)/(double)2;
+        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
         System.out.println("Area of your triangle is: "+area);
+
     }
 }
